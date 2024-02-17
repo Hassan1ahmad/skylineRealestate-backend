b703c3568aa4b1dd4057e7cc3b3ba1698a6778b9
Hassan Ahmad <hassan1ahmad999@gmail.com>
2024-02-17 14:31:28 +0500

    solving cors error

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 
 const cors= (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // '*' allows any origin, replace with your specific domain in production
+  res.header('Access-Control-Allow-Origin', 'https://skylinerealestate.netlify.app'); // '*' allows any origin, replace with your specific domain in production
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
